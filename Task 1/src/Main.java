c10719839a45104e26a07c315adf246f80a96d9e
XandrRF <xandrnf@gmail.com>
2024-12-20 22:02:59 +0500

    готово

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -15,22 +17,27 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+        System.out.println("вычитания чисел " + (a - b));
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        System.out.println("деления чисел " + (a / b));
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        System.out.println("умножения чисел " + (a * b));
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        System.out.println("суммирования чисел " + (a + b));
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextDouble();
     }
 }
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        check();
     }
+
+    private static int input() {
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+
+    private static void check() {
+        System.out.println((input() % 2) == 0);
+    }
+
 }
